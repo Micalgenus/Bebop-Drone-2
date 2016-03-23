a8648990729f1b48cc5483eaba34137a7b5826d2
Micalgenus <rudtn5140@gmail.com>
2016-03-23 19:54:42 +0900

    modified:   Data_Extract.py 	modified:   PLF_Analyzer.py

--- Data_Extract.py
+++ Data_Extract.py
@@ -4,6 +4,9 @@ import gzip
 import os
 import random
 
+#def Link():
+#	
+
 def Extract(data, start, end):
 #	print start, end
 #	tmp_file = result_dir + "tmp." + str(random.randrange(1000000, 9999999))
@@ -26,7 +29,9 @@ def Extract(data, start, end):
 		os.remove(tmp_file_gz)
 		error = False
 	else:
-		error = True
+#		error = True
+		error = False
+		tmp_file = tmp_file_gz
 	
 #	os.remove(tmp_file_gz)
 	return error, tmp_file
@@ -68,12 +73,39 @@ def MakeFile(tfile, path):
 			i += 1
 		except IndexError:
 			f.close()
+
 			directory = path + "/" + os.path.dirname(file_name)
 			filename = os.path.basename(file_name)
 			if not os.path.exists(directory):
 				os.makedirs(directory)
+
+			if file_dummy == "\xFF\xA1":
+#				Link
+#				print file_data[len(file_data) - 1].encode('hex')
+				if file_data[len(file_data) - 1] == "\x00":
+#					os.system("ln -s " + path + "/" + filename + " " + file_data[:len(file_data) - 2])
+					os.system("ln -s " + file_data[:len(file_data) - 1] + " " + path + "/" + file_name)
+#					os.symlink(filename, file_data[:len(file_data) - 2])
+#					file_data.strip('\x00')
+#					file_data.replace("\x00", "")
+#				print file_data[len(file_data) - 2].encode('hex')
+				else:
+					os.system("ln -s " + file_data + " " + path + "/" + file_name)
+#					os.symlink(filename, file_data)
+
+				os.remove(tfile)
+				return None
+
+			elif file_dummy == "\xED\x41":
+#				Directory
+				os.makedirs(path + "/" + file_name)
+#				print file_data.encode('hex')
+				os.remove(tfile)
+				return None
+
 #			f = open(directory + "/" + filename + "_" + file_dummy.encode('hex'), "wb")
 			f = open(directory + "/" + filename, "wb")
+#			print directory + "/" + filename
 			f.write(file_data)
 			f.close()
 			os.remove(tfile)
--- PLF_Analyzer.py
+++ PLF_Analyzer.py
@@ -17,7 +17,7 @@ def Init():
 		print "Usage: " + sys.argv[0] + " FILE [RESULT]"
 		print "	FILE: PLF File"
 		print "	RESULT: Make result directory"
-		print "					default: plf"
+		print "         default: plf"
 		sys.exit()
 	if argc >= 2:
 		try:
@@ -32,7 +32,7 @@ def Init():
 	if not os.path.exists(result_dir):
 		os.mkdir(result_dir)
 	else:
-		print "Directory does exist"
+		print " Directory does exist"
 		sys.exit()
 
 	result = open(result_dir + "/log.txt", "wb")
@@ -62,7 +62,7 @@ def sPLFFile(index):
 	return index
 	
 def sPLFEntryTag(index):
-	result.writelines("====== PLFEntryTag("+str(index)+") ======\n")
+	result.writelines("====== PLFEntryTag(" + str(index)+ ") ======\n")
 	result.writelines("dwSectionType : "+ bin[index:index+4].encode("hex") + '\n');index+=4
 	result.writelines("dwEntrySize : "+ bin[index:index+4].encode("hex") + '\n')
 	next_index=int(EndianConvert(bin[index:index+4]));index+=4
@@ -82,19 +82,25 @@ if __name__ == "__main__":
 			index = sPLFFile(index)
 #		elif bin[index:index+4].encode("hex") == "00000000" or "00000003" or "00000007" or "00000009" or "0000000b" or "0000000c":
 		elif header == "00000000" or "00000003" or "00000007" or "00000009" or "0000000b" or "0000000c":
-			if index%4 != 0:
-				index += 4-(index%4)
 			(index, size) = sPLFEntryTag(index)
 			start = index
 			end = index + size
 
+#			if bin[index:index + 4] == "PLF!":
+#				print "PLF!"
+#				os.system("dd if=" + sys.argv[1] + " bs=1 skip=" + str(index) + " count=" + str(size) + " of=plf" + str(index) + ".plf")
+#			print header
 			index += size
-			if header == "00000009":
+			if header == "09000000":
 #				print start, end
 				(error, tmp_file) = Extract(bin, start, end)
 				if not error:
 					MakeFile(tmp_file, result_dir + "/rootfs")
 #				MakeFile(
+
+			if index%4 != 0:
+				index += 4-(index%4)
+
 			if file_size <= index:
 				break
 	
